45bf9f95150a5fe68f140c8e961b1f3896d16444
juno427 <76770110+juno427@users.noreply.github.com>
2021-10-07 12:06:09 +0900

    05_조인.sql

--- 05_조인.sql
+++ 05_조인.sql
@@ -29,4 +29,37 @@ SELECT e.last_name 직원, m.last_name 매니저
 FROM employees e
 JOIN employees m ON e.manager_id = m.employee_id;
 
+-- 외부 조인
+-- 기본조인(부서번호가 null값인 직원은 제외)
+SELECT e.last_name 이름, d.department_id 부서번호, d.department_name 부서명
+FROM employees e JOIN departments d ON e.department_id = d.department_id;
+
+SELECT *
+FROM employees
+WHERE department_id is NULL; --부서번호가 null인 직원
+
+-- LEFT 외부 조인 : 직원 테이블 중에 부서가 없는 직원도 출력
+SELECT e.last_name 이름, d.department_id 부서번호, d.department_name 부서명
+FROM employees e LEFT OUTER JOIN departments d ON e.department_id = d.department_id;
+
+-- RIGHT 외부 조인 : 부서테이블에 있지만 직원테이블에서 사용하지 않는 부서도 전부 출력됨
+SELECT e.last_name 이름, d.department_id 부서번호, d.department_name 부서명
+FROM employees e RIGHT OUTER JOIN departments d ON e.department_id = d.department_id;
+
+-- FULL 외부 조인 : 조건에 맞지 않는 모든 데이터 줄을 출력함
+SELECT e.last_name 이름, d.department_id 부서번호, d.department_name 부서명
+FROM employees e FULL OUTER JOIN departments d ON e.department_id = d.department_id;
+
+-- 예제) countries, locations 테이블을 조인하여 아래와 같이 출력하시오.
+SELECT c.country_name 국가, c.country_id 국가번호, l.location_id 지역번호, l.city 도시
+FROM countries c LEFT OUTER JOIN locations l ON c.country_id = l.country_id
+ORDER BY 지역번호 DESC;
+
+-- 예제) countries, regions 테이블을 크로스 조인하여 아래와 같이 각각의 테이블의 25행 * 4행 => 100행의 결과가 나오도록 하세요.
+SELECT c.country_name 국가, r.region_name 지역이름
+FROM countries c
+CROSS JOIN regions r;
+
+
+
 
