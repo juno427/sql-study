0cc8e56ba6ab054a5f85d270b7b81b3b50649f8f
JW <tkrkwl333@gmail.com>
2021-10-21 16:55:20 +0900

    2021-10-21

--- 09_제약조건.sql
+++ 09_제약조건.sql
@@ -5,39 +5,39 @@ DESC departments;
 
 -- UK 유니크 : 중복되지 않음
 
-CREATE TABLE emp (
-    eno         NUMBER(3),
-    emp_name    VARCHAR2(20) CONSTRAINT emp1_ename_nn NOT NULL,
-    email       VARCHAR2(30),
+-- emp_name은 NOT NULL 제약조건, email은 유니크 제약조건
+CREATE TABLE emp1 (
+    eno      NUMBER(3),
+    emp_name VARCHAR2(20) CONSTRAINT emp1_ename_nn NOT NULL,
+    email    VARCHAR2(30),
     CONSTRAINT emp1_email_uk UNIQUE (email)
 );
-
 -- 데이터를 입력
 INSERT INTO emp1 VALUES(208, 'Kim', 'KJS@naver.com');
-INSERT INTO emp1 VALUES(208, 'NULL', 'ABC@naver.com'); -- 널값을 입력할 수 없어서 에러발생
-INSERT INTO emp1 VALUES(208, 'Lee', 'KJS@naver.com'); -- 중복 안됨 email 유니크 (유니크 제약조건 어김)
+INSERT INTO emp1 VALUES(209, NULL, 'ABC@naver.com'); -- 널값을 입력할수 없어서 에러발생
+INSERT INTO emp1 VALUES(210, 'Lee', 'KJS@naver.com'); -- 중복 안됨 email 유니크
 
 -- Check 제약조건
 CREATE TABLE emp2 (
-    eno         NUMBER(3),
-    emp_name    VARCHAR2(20),
-    sal         NUMBER(10),
+    eno      NUMBER(3),
+    emp_name VARCHAR2(20),
+    sal      NUMBER(10),
     CONSTRAINT emp2_sal_check check(sal>1000)
 );
 INSERT INTO emp2 VALUES(208, 'Kim', 1200);
-INSERT INTO emp2 VALUES(209, 'Lee', 900); -- sal의 값이 1000보다 커야 된다.
+INSERT INTO emp2 VALUES(209, 'Lee', 900);  -- sal의 값이 1000보다 커야 된다.
 
 -- 제약조건 이름 없이 테이블 생성
 DROP TABLE emp;
 CREATE TABLE emp (
     eno NUMBER(4) PRIMARY KEY,
-    ename VARCHAR(20) NOT NULL,
+    ename VARCHAR2(20) NOT NULL,
     gno VARCHAR2(13) UNIQUE CHECK (LENGTH(gno) >= 8),
-    gender VARCHAR2(5) CHECK (gender IN('woman', 'man'))
+    gender VARCHAR2(5) CHECK (gender IN('woman','man'))
 );
 
-INSERT INTO emp VALUES(1, 'Kim', '12345678','man');
-INSERT INTO emp VALUES(2, 'Kim', '123456789','woman');
+INSERT INTO emp VALUES(1,'Kim','12345678','man');
+INSERT INTO emp VALUES(2,'Kim','123456789','woman');
 
 -- 예제 1
 DROP TABLE members;
@@ -51,13 +51,4 @@ CREATE TABLE members (
 );
 
 DESC members;
-
-
-
-
-
-
-
-
-
-
+    
